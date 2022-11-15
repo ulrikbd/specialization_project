1dfd774fbad6d080aaaa522073549ee105996e8b
ulrikbd <ulrik_danielsen@hotmail.com>
2022-11-15 10:49:18 +0100

    Started writing discussion about bandwith choice

--- code/full_pipeline.py
+++ code/full_pipeline.py
@@ -2,10 +2,12 @@ import pickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
 
 from behavioral_clustering import BehavioralClustering
 from helper_functions import (
         plot_scaleogram, assign_labels,
+        get_contours, plot_watershed_heat,
 )
 
 
@@ -62,12 +64,72 @@ def load_pipeline():
     return bc
 
 
-def main():
-    pickle_pipeline()
+def perplexity_tuning_full():
+    """
+    Perform the clustering on the full pipeline
+    varying the perplexity parameter i t-SNE.
+    """
+
+    # Perplexity values to be tested
+    perp = [1, 5, 30, 50, 200, 500]
+
     bc = load_pipeline()
-    print(bc.embedded.shape)
-    print(bc.beh_labels.shape)
-    print(bc.fit_pca.shape)
+    bc.bw = 0.01
+
+    plt.figure(figsize = (12, 8))
+    # Iterate over chosen perplexities
+    for i in range(len(perp)):
+        bc.perp = perp[i]
+        print("Perplexity:", perp[i])
+        bc.tsne()
+        bc.kernel_density_estimation(100j)
+        bc.watershed_segmentation()
+
+        contours = get_contours(bc.kde, bc.ws_labels)
+        
+        plt.subplot(2, 3, i + 1)
+        plt.title("Perplexity = " + str(perp[i]))
+        plot_watershed_heat(bc.embedded, bc.kde,
+                            contours, bc.border)
+    plt.savefig("./figures/perplexity_tuning_full_pipeline.pdf", 
+                bbox_inches = "tight")
+
+
+def bandwidth_tuning():
+    """
+    Plot the resulting clusters for several values of
+    the bandwidth parameter used in the kernel 
+    density estimation.
+    """
+
+    bc = load_pipeline()
+
+    bandwidths = np.logspace(-5, -1, 10)
+
+    for bandwidth in bandwidths:
+        bc.bw = bandwidth
+
+        bc.tsne()
+        bc.kernel_density_estimation(100j)
+        bc.watershed_segmentation()
+
+        contours = get_contours(bc.kde, bc.ws_labels)
+        
+        plt.figure()
+        plt.title("Bandwidth = " + str(bandwidth))
+        plot_watershed_heat(bc.embedded, bc.kde,
+                            contours, bc.border)
+        plt.savefig("./figures/bandwidth_tuning/clustering_bw_" + str(bc.bw) + ".pdf", 
+                bbox_inches = "tight")
+
+
+def main():
+    sns.set_theme()
+    # pickle_pipeline()
+    bc = load_pipeline()
+
+    # perplexity_tuning_full()
+    bandwidth_tuning()
     
 
 if __name__ == "__main__":
